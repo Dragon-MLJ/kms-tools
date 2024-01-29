daec3185c61121c31492a628b6caf9e32e8cc675
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-29 20:57:43 +0800

    Create tools.ts

---
+++ src/locales/zh-CN/tools.ts
@@ -0,0 +1,23 @@
+export default {
+    button: {
+      submit: '提交',
+    },
+    card: {
+      title: 'KMS 服务器状态检测',
+    },
+    form: {
+      label: {
+        domain: 'KMS主机',
+        port: 'KMS端口',
+        protocol: '激活协议',
+        software: '激活产品',
+      },
+      placeholder: {
+        domain: '请输入KMS主机',
+        port: '请输入KMS端口',
+        protocol: '请选择协议',
+        software: '请选择产品',
+      },
+    },
+  }
+  
