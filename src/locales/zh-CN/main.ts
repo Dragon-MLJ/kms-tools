96e957c00982e645ff426f3e6de5ac86698e8c2b
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-29 20:57:27 +0800

    Create main.ts

---
+++ src/locales/zh-CN/main.ts
@@ -0,0 +1,18 @@
+export default {
+    author: '-MLJ-',
+    module: {
+      activate: '激活',
+      monitor: '监控',
+      tools: '工具箱',
+    },
+    theme: {
+      auto: '自动模式',
+      dark: '深色模式',
+      light: '浅色模式',
+    },
+    languages: {
+      en: '英语',
+      zhCn: '简体中文',
+    },
+  }
+  
