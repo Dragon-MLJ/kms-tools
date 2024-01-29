b28681227404c35751d1030bef3fa388e7e1519f
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-29 19:33:06 +0800

    Update custom-footer.vue

--- src/components/custom-footer.vue
+++ src/components/custom-footer.vue
@@ -12,12 +12,9 @@ const { t } = useI18n()
       <ASpace>
         <div>
           Copyright &copy; {{ currentYear }}
-          <ALink target="_blank" href="https://github.com/ikxin/kms-tools"> KMS Tools </ALink>
+        
         </div>
-        <div>
-          Code with by
-          <ALink target="_blank" href="https://www.ikxin.com">{{ t('main.author') }}</ALink>
-        </div>
+       
       </ASpace>
     </div>
   </ALayoutFooter>
