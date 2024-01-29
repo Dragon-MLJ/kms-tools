85fb2480e43c175b3fcbc4614198d5670976ab03
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-29 20:59:55 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { router } from '@/router'
 import { createI18n } from 'vue-i18n'
 import messages from '@/locales'
 import { useMonitorStore } from '@/store/monitor'
+import en from '@/locales/en/index'
+import zh from './locales/zh-CN/index'
 
 import '@/styles/custom.less'
 import 'virtual:uno.css'
@@ -10,7 +12,7 @@ import '@/styles/style.css'
 
 const app = createApp(App)
 const pinia = createPinia()
-const i18n = createI18n({ locale: 'en', messages })
+const i18n = createI18n({ locale: 'zh', messages:{zh,en})
 
 app.use(pinia)
 app.use(router)
