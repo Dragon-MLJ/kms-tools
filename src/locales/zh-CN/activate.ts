28f28c8ed121f27e6f6f60fef3f297a60a2e7318
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-29 20:57:07 +0800

    Create activate.ts

---
+++ src/locales/zh-CN/activate.ts
@@ -0,0 +1,43 @@
+export default {
+    button: {
+      copy: '复制脚本',
+      create: '创建脚本',
+      download: '下载脚本',
+    },
+    form: {
+      label: {
+        edition: '发行版本',
+        cpuarch: 'CPU架构',
+        license: '许可证密钥',
+        product: '产品版本',
+        service: 'KMS 服务器',
+        version: '系统版本',
+      },
+      placeholder: {
+        edition: '请选择发行版本',
+        product: '请选择产品版本',
+        service: '请选择KMS服务器',
+        version: '请选择系统版本',
+      },
+    },
+    message: {
+      error: '复制失败',
+      success: '复制成功',
+    },
+    radio: {
+      x64: 'x64',
+      x86: 'x86',
+    },
+    sider: {
+      system: '系统',
+      software: '软件',
+    },
+    table: {
+      columns: {
+        edition: '发行版本',
+        license: '许可证密钥',
+        product: '产品版本',
+      },
+    },
+  }
+  
